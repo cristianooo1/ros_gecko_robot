3d836b75c82ddaf1973de10e2fe310da5e8b65d1
cristianooo <cristi_iorga02@yahoo.com>
2025-08-31 17:09:38 +0300

    Converted the IK cpp code into a service node. Created a custom request of .srv type.

--- src/cpp_pkg/src/cpp_IK.cpp
+++ src/cpp_pkg/src/cpp_IK.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string>
+#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/qos.hpp"
@@ -11,77 +12,85 @@
 #include "kdl/chainiksolverpos_lma.hpp"
 #include "kdl_parser/kdl_parser.hpp"
 
-using std::placeholders::_1;
-
+#include "cpp_pkg/srv/compute_leg_ik.hpp"
 
 class LegIK : public rclcpp::Node
 {
 public:
   LegIK() : Node("leg_inverse_kinematics_example")
   {
-    // Subscribe to URDF
+
     subscription_ = this->create_subscription<std_msgs::msg::String>(
       "robot_description",
       rclcpp::QoS(rclcpp::KeepLast(1))
         .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL),
-      std::bind(&LegIK::robotDescriptionCallback, this, _1));
+      std::bind(&LegIK::robotDescriptionCallback, this, std::placeholders::_1));
+
+    service_ = this->create_service<cpp_pkg::srv::ComputeLegIK>(
+      "/compute_leg_ik",
+      std::bind(&LegIK::computeLegIKCallback, this,
+                std::placeholders::_1, std::placeholders::_2));
+
+    RCLCPP_INFO(this->get_logger(), "Leg IK Service Server is ready.");
   }
 
 private:
-  //!
-  //! Construct KDL IK solver from URDF string
-  void robotDescriptionCallback(const std_msgs::msg::String& msg)
+  
+  void computeLegIKCallback(
+    const std::shared_ptr<cpp_pkg::srv::ComputeLegIK::Request> request,
+    std::shared_ptr<cpp_pkg::srv::ComputeLegIK::Response> response)
   {
-    // Construct KDL tree from URDF
-    const std::string urdf = msg.data;
-    kdl_parser::treeFromString(urdf, tree_);
-    // Print basic information about the tree
-    std::cout << "nb joints:        " << tree_.getNrOfJoints() << std::endl;
-    std::cout << "nb segments:      " << tree_.getNrOfSegments() << std::endl;
-    std::cout << "root segment:     " << tree_.getRootSegment()->first
-              << std::endl;
-    // Get kinematic chain of the leg
-    tree_.getChain("base_link", "leg_tip_1", chain_);
-    std::cout << "chain nb joints:  " << chain_.getNrOfJoints() << std::endl;
-    // Create IK solver
+
+    tree_.getChain("base_link", request->leg_tip, chain_);
+    std::cout << "Chain number of joints:  " << chain_.getNrOfJoints() << std::endl;
+
     solver_ = std::make_unique<KDL::ChainIkSolverPos_LMA>(chain_);
 
-    // Run usage example
-    usageExample();
+    double knee_angle = request->curr_knee_joint;
+    double ankle_angle = request->curr_ankle_joint;
+    getJointAngles(request->target_foot_pos.x, request->target_foot_pos.y, request->target_foot_pos.z, knee_angle, ankle_angle);
+    response->tar_knee_joint = knee_angle;
+    response->tar_ankle_joint = ankle_angle;
+    response->success_flag = true;
+    // RCLCPP_INFO(this->get_logger(), "Incoming request: x=%.2f, y=%.2f, z=%.2f",
+    //             request->target_foot_pos.x, request->target_foot_pos.y, request->target_foot_pos.z);
+    // RCLCPP_INFO(this->get_logger(), "Sending back response: knee_angle=%.2f, ankle_angle=%.2f",
+    //             response->tar_knee_joint, response->tar_ankle_joint);
   }
 
-  //!
-  //! Cartesian x, z => hip and knee joint angles
-  void getJointAngles(
-    const double x, const double y, const double z, double & hip_angle, double & knee_angle)
+
+
+  void robotDescriptionCallback(const std_msgs::msg::String& msg)
   {
-    // Prepare IK solver input variables
+
+    const std::string urdf = msg.data;
+    kdl_parser::treeFromString(urdf, tree_);
+
+    std::cout << "Number of joints: " << tree_.getNrOfJoints() << std::endl;
+    std::cout << "Number of segments: " << tree_.getNrOfSegments() << std::endl;
+    std::cout << "Root segment: " << tree_.getRootSegment()->first
+              << std::endl;
+  }
+
+
+  void getJointAngles(
+    const double x, const double y, const double z, double & knee_angle, double & ankle_angle)
+  {
+
     KDL::JntArray q_init(chain_.getNrOfJoints());
-    q_init(0) = 0.0;
-    q_init(1) = 0.0;
+    q_init(0) = knee_angle;
+    q_init(1) = ankle_angle;
     const KDL::Frame p_in(KDL::Vector(x, y, z));
     KDL::JntArray q_out(chain_.getNrOfJoints());
-    // Run IK solver
+
     solver_->CartToJnt(q_init, p_in, q_out);
-    // Write out
-    hip_angle = q_out(0);
-    knee_angle = q_out(1);
+
+    knee_angle = q_out(0);
+    ankle_angle = q_out(1);
   }
 
-  //!
-  //! Calculate and print the joint angles
-  //! for moving the foot to {x: 0.3, y=0.1, z: -0.3}
-  void usageExample()
-  {
-    double hip_angle, knee_angle;
-    getJointAngles(0.320609, -0.148914, -0.0176756, hip_angle, knee_angle);
-    printf("knee and hip joint angles: %.1f, %.1f °\n",
-           hip_angle*180/M_PI, knee_angle*180/M_PI);
-    fflush(stdout);
-  }
-
-  // Class members
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Service<cpp_pkg::srv::ComputeLegIK>::SharedPtr service_;
   KDL::Tree tree_;
   KDL::Chain chain_;
   std::unique_ptr<KDL::ChainIkSolverPos_LMA> solver_;
